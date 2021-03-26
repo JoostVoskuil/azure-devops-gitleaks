7d2210bfc0a5a06dab775aea0efab39a9aa2ce69
Joost Voskuil <joost@foxhole.nl>
2021-03-26 21:42:41 +0100

    tab fix

--- task/index.ts
+++ task/index.ts
@@ -3,76 +3,76 @@ import tr = require('azure-pipelines-task-lib/toolrunner');
 import { GitleaksTool } from './gitleakstool';
 
 async function run() {
-        try {
-                console.log(`Thanks to Zachary Rice (https://github.com/zricethezav) for creating and maintaining gitleaks.`);
-                console.log(`Thanks to Jesse Houwing (https://github.com/jessehouwing) for providing a gitleaks config that has most of Microsoft's deprecated credscan rules ported to it.`);
-                console.log();
+	try {
+		console.log(`Thanks to Zachary Rice (https://github.com/zricethezav) for creating and maintaining gitleaks.`);
+		console.log(`Thanks to Jesse Houwing (https://github.com/jessehouwing) for providing a gitleaks config that has most of Microsoft's deprecated credscan rules ported to it.`);
+		console.log();
 
-                const operatingSystem = getAzureDevOpsVariable('Agent.OS');
-                const architecture = getAzureDevOpsVariable('Agent.OSArchitecture');
-                const agentTempDirectory = getAzureDevOpsVariable('Agent.TempDirectory');
+		const operatingSystem = getAzureDevOpsVariable('Agent.OS');
+		const architecture = getAzureDevOpsVariable('Agent.OSArchitecture');
+		const agentTempDirectory = getAzureDevOpsVariable('Agent.TempDirectory');
 
-                const specifiedVersion = taskLib.getInput('version') || 'latest';
-                const scanfolder = getAzureDevOpsInput('scanfolder');
-                const configType = taskLib.getInput('configtype') || 'default';
+		const specifiedVersion = taskLib.getInput('version') || 'latest';
+		const scanfolder = getAzureDevOpsInput('scanfolder');
+		const configType = taskLib.getInput('configtype') || 'default';
 
-                const predefinedConfigFile = taskLib.getInput('predefinedconfigfile');
-                const customConfigFile = taskLib.getInput('configfile');
+		const predefinedConfigFile = taskLib.getInput('predefinedconfigfile');
+		const customConfigFile = taskLib.getInput('configfile');
 
-                const gitleaksTool: GitleaksTool = new GitleaksTool('gitleaks', specifiedVersion, operatingSystem, architecture);
-                const configFileParameter = gitleaksTool.getGitLeaksConfigFileParameter(configType, predefinedConfigFile, customConfigFile);
-                const reportPath = gitleaksTool.getGitleaksReportPath(agentTempDirectory);
+		const gitleaksTool: GitleaksTool = new GitleaksTool('gitleaks', specifiedVersion, operatingSystem, architecture);
+		const configFileParameter = gitleaksTool.getGitLeaksConfigFileParameter(configType, predefinedConfigFile, customConfigFile);
+		const reportPath = gitleaksTool.getGitleaksReportPath(agentTempDirectory);
 
-                const cachedTool = await gitleaksTool.getTool();
-                const toolRunner: tr.ToolRunner = new tr.ToolRunner(cachedTool);
+		const cachedTool = await gitleaksTool.getTool();
+		const toolRunner: tr.ToolRunner = new tr.ToolRunner(cachedTool);
 
-                console.debug(`Scan Folder is set to ${scanfolder}`);
-                console.debug(`Report path is set to ${reportPath}`);
+		console.debug(`Scan Folder is set to ${scanfolder}`);
+		console.debug(`Report path is set to ${reportPath}`);
 
-                //Replaces Windows \ because of bug in TOML Loader
-                toolRunner.arg([`--path=${scanfolder.replace(/\\/g, '/')}`]);
-                toolRunner.arg([`--report=${reportPath.replace(/\\/g, '/')}`]);
-                if (configFileParameter) toolRunner.arg([`${configFileParameter}`]);
+		//Replaces Windows \ because of bug in TOML Loader
+		toolRunner.arg([`--path=${scanfolder.replace(/\\/g, '/')}`]);
+		toolRunner.arg([`--report=${reportPath.replace(/\\/g, '/')}`]);
+		if (configFileParameter) toolRunner.arg([`${configFileParameter}`]);
 
-                if (taskLib.getBoolInput('nogit')) toolRunner.arg([`--no-git`]);
-                if (taskLib.getBoolInput('verbose')) toolRunner.arg([`--verbose`]);
+		if (taskLib.getBoolInput('nogit')) toolRunner.arg([`--no-git`]);
+		if (taskLib.getBoolInput('verbose')) toolRunner.arg([`--verbose`]);
 
-                // Set options to run the toolRunner
-                const options: tr.IExecOptions = {
-                        failOnStdErr: false,
-                        ignoreReturnCode: true,
-                        silent: false,
-                        outStream: process.stdout,
-                        errStream: process.stderr
-                };
+		// Set options to run the toolRunner
+		const options: tr.IExecOptions = {
+			failOnStdErr: false,
+			ignoreReturnCode: true,
+			silent: false,
+			outStream: process.stdout,
+			errStream: process.stderr
+		};
 
-                const result: number = await toolRunner.exec(options);
+		const result: number = await toolRunner.exec(options);
 
-                if (taskLib.exist(reportPath) && taskLib.getBoolInput('uploadresults')) {
-                        taskLib.uploadArtifact('gitleaks', reportPath, 'gitleaks');
-                }
+		if (taskLib.exist(reportPath) && taskLib.getBoolInput('uploadresults')) {
+			taskLib.uploadArtifact('gitleaks', reportPath, 'gitleaks');
+		}
 
-                if (result === 0) {
-                        taskLib.setResult(taskLib.TaskResult.Succeeded, '');
-                } else {
-                        taskLib.setResult(taskLib.TaskResult.Failed, 'Leaks or error encountered. See log and report for details.');
-                }
-        }
-        catch (err) {
-                taskLib.setResult(taskLib.TaskResult.Failed, err);
-        }
+		if (result === 0) {
+			taskLib.setResult(taskLib.TaskResult.Succeeded, '');
+		} else {
+			taskLib.setResult(taskLib.TaskResult.Failed, 'Leaks or error encountered. See log and report for details.');
+		}
+	}
+	catch (err) {
+		taskLib.setResult(taskLib.TaskResult.Failed, err);
+	}
 }
 
 run();
 
 function getAzureDevOpsVariable(name: string): string {
-        const value = taskLib.getVariable(name) || undefined;
-        if (value === undefined) throw Error(`Variable ${name} is empty`);
-        return value;
+	const value = taskLib.getVariable(name) || undefined;
+	if (value === undefined) throw Error(`Variable ${name} is empty`);
+	return value;
 }
 
 function getAzureDevOpsInput(name: string): string {
-        const value = taskLib.getInput(name) || undefined;
-        if (value === undefined) throw Error(`Input ${name} is empty`);
-        return value;
+	const value = taskLib.getInput(name) || undefined;
+	if (value === undefined) throw Error(`Input ${name} is empty`);
+	return value;
 }
