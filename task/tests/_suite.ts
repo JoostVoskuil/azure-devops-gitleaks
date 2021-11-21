4a5e7da9dc6f87ec014d2405a2ee8d4d5e3a057f
Joost Voskuil <joost@foxhole.nl>
2021-11-21 14:57:27 +0100

    test fixes

--- task/tests/_suite.ts
+++ task/tests/_suite.ts
@@ -219,7 +219,7 @@ describe('Gitleaks versions', function () {
         tr.run();
         assert.strictEqual(tr.succeeded, true, 'should have succeeded');
         assert.strictEqual(tr.invokedToolCount, 1, 'Gitleaks tool should be invoked 1 time');
-        assert.strictEqual(tr.stdout.indexOf('https://github.com/zricethezav/gitleaks/releases/download/v7.0.0/') >= 0, true, "Should contain 'https://github.com/zricethezav/gitleaks/releases/download/v7.0.0/' (that is specified as latest)")
+        assert.strictEqual(tr.stdout.indexOf('loc_mock_OnlineAgentHasNotTheLatestVersion') >= 0, true, "Should contain 'loc_mock_OnlineAgentHasNotTheLatestVersion.'")
         done();
     });
     it('Should get version that is specified.', function(done: Mocha.Done) {    
@@ -228,7 +228,7 @@ describe('Gitleaks versions', function () {
         tr.run();
         assert.strictEqual(tr.succeeded, true, 'should have succeeded');
         assert.strictEqual(tr.invokedToolCount, 1, 'Gitleaks tool should be invoked 1 time');
-        assert.strictEqual(tr.stdout.indexOf('https://github.com/zricethezav/gitleaks/releases/download/v6.0.0/') >= 0, true, "Should contain 'https://github.com/zricethezav/gitleaks/releases/download/v9.0.0/'")
+        assert.strictEqual(tr.stdout.indexOf('loc_mock_NoToolcacheDownloading') >= 0, true, "Should contain 'loc_mock_NoToolcacheDownloading.'")
         done();
     });
 });
