01aa8f1a7dba13ee2113935f2cabe20bb31fe63a
Joost Voskuil <joost@foxhole.nl>
2023-12-15 16:03:27 +0100

    fixes

--- task/v2/index.ts
+++ task/v2/index.ts
@@ -38,7 +38,9 @@ async function run(): Promise<void> {
     toolRunner.arg(['detect'])
     toolRunner.argIf(getConfigFilePath(), [`--config=${getConfigFilePath()}`])
     toolRunner.arg([`--source=${scanLocation}`])
-    toolRunner.argIf(logOptions, `--log-opts=${logOptions}`)
+    if (logOptions) {
+      toolRunner.line(`--log-opts=${logOptions}`)
+    }
     toolRunner.argIf(taskLib.getBoolInput('redact'), ['--redact'])
     toolRunner.argIf(debug === 'true', ['--log-level=debug'])
     toolRunner.arg([`--report-format=${reportFormat}`])
